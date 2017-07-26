3f0d3955de364ebd0bce8c9061b3623b88512af2
Claire <chiu520109@gmail.com>
2017-07-26 23:07:07 +0800

    新增學生管理套件

---
+++ Lecture01-Examples/Example02/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Example02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Student aStudent = new Student()
+            {
+                name = "Claire",
+                phone = "123",
+                address = "台大"
+            };
+            Console.WriteLine(
+                "學生姓名:{0}, 電話:{1}, 住址:{2}"
+                , aStudent.name
+                , aStudent.phone
+                , aStudent.address
+             );
+        }
+    }
+}
